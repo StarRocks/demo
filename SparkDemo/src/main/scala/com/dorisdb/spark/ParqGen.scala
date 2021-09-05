b81c3b1822d3a02033d78bc5a59ea1214c620fa8
Simon.Xie <thimoon@sina.com>
2021-09-05 23:02:02 +0800

    ParqGen distributedly

--- SparkDemo/src/main/scala/com/dorisdb/spark/ParqGen.scala
+++ SparkDemo/src/main/scala/com/dorisdb/spark/ParqGen.scala
@@ -13,38 +13,36 @@
 
 package com.dorisdb.spark
 
-import com.dorisdb.utils.{Consts, LoggerUtil, MyDorisSink}
-import org.apache.spark.{SparkConf, TaskContext}
+import com.dorisdb.utils.LoggerUtil
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+import org.apache.spark.{Partitioner, SparkConf}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 object ParqGen {
-  // parameters
-  val master = "local"
   val appName = "app_spark_gen"
 
   LoggerUtil.setSparkLogLevels()
 
-  import org.apache.spark.sql.{SaveMode, SparkSession}
-  import scala.util.Random
-
-  def genRand(s:Int = 10000): Int = {
-    new Random().nextInt(s)
-  }
-
-  def getLine(cols:Int = 10): String ={
-      val tpl = "%s\t"
-      var line = ""
+  def getLine(cols:Int = 10) : List[Int] ={
+      val line = new ArrayBuffer[Int]()
+      val rand = new Random
       for (x <- Range(0, cols, 1) ) {
-        line = line + tpl.format(genRand(x + 1024))
+        line append rand.nextInt(x + 1024)
       }
-      line = line + genRand(cols + 10).toString
-      line
+      val res = line.toList
+      line.clear()
+      res
   }
 
   def getTable(lines:Int = 10,  cols:Int = 10) :String = {
+      val arr = getLine(cols)
       val tpl = "%s\n"
       var table = ""
-      for (x<- Range(0, lines, 1 )) {
-        table = table + tpl.format( getLine(cols))
+      val rand = new Random
+      for ( _ <- Range(0, lines, 1 )) {
+        table = table + tpl.format( rand.shuffle(arr).mkString("\t"))
       }
       table.stripMargin('\n')
   }
@@ -52,44 +50,64 @@ object ParqGen {
 
   def main(args: Array[String]): Unit = {
 
+    // Args
+    val sinkBuckets = if (args.length > 0  ) args(0).toInt else 5
+    val pqPath = if (args.length > 1 ) args(1) else  "/simon_test/t1"
+    val bucketsLines = if (args.length > 2 ) args(2).toInt else 1000
+    val genDup = if (args.length > 3 ) args(3).toInt else 5
+    val loop = if (args.length > 4 ) args(4).toInt else 5
+    val computeBuckets = if (args.length > 5  ) args(5).toInt else 5
+    val master = if (args.length > 6  ) args(6) else "local"
+    val cols = if (args.length > 7 ) args(7).toInt else 400
+
+    // Env
     val conf = new SparkConf()
       .setAppName(appName)
       .setMaster(master)
       .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
-    val spark = SparkSession.builder().config(conf).master(master).enableHiveSupport().getOrCreate()
+      .set("spark.debug.maxToStringFields","10000")
+    val spark = SparkSession.builder().config(conf).master(master).getOrCreate()
     val sc = spark.sparkContext
     sc.setLogLevel("WARN")
     import spark.implicits._
+    import org.apache.spark.sql.types._
 
-    val csv_temp = if (args.length > 0  ) args(0) else "hdfs://mycluster:9002/simon_test/csv_temp"
-    val pq_path = if (args.length > 1 ) args(1) else  "hdfs://mycluster:9002/simon_test/t1"
-    val total_lines = if (args.length > 2 ) args(2).toLong else 130000000l
-    val batch_lines = if (args.length > 3 ) args(3).toInt else 1000000
-    val cols = if (args.length > 4 ) args(4).toInt else 80 + 167 + 45 + 23
+    // base * genDup
+    // Cost Tips: generates 1k x 4 lines takes 4 mins, size 17MB
+    var rdd = sc.parallelize(Range(0,computeBuckets))
+      .map(x => (x,x))
+      .partitionBy(new Partitioner {
+        override def numPartitions: Int = computeBuckets
+        override def getPartition(key: Any): Int = key.toString.toInt
+      })
+      .mapPartitions(itr=>{
+        itr.map( _ => {
+          val base = getTable(bucketsLines, cols)
+          (base * genDup).stripMargin('\n')
+        } )
+      })
 
-    var i = 1
-    while (i < total_lines) {
-      if (i% 13 == 1) {  // real getTable() calls
-        // clear
-        try {
-          val hadoopConf = new org.apache.hadoop.conf.Configuration()
-          val hdfs = org.apache.hadoop.fs.FileSystem.get(new java.net.URI(csv_temp), hadoopConf)
-          hdfs.delete(new org.apache.hadoop.fs.Path(csv_temp), true)
-        } catch { case _ : Exception => { }}
-
-        // Randomly generate 10000 rows, multiply 100 times and generate CSV file
-        val data = getTable(batch_lines/100, cols)
-        val dtimes = ((data+"\n" )* 100).stripSuffix("\n")
-        val res = sc.parallelize(dtimes.split("\n"))
-        res.saveAsTextFile(csv_temp)
-      }
-
-      // 1000000 lines csv to parquet
-      spark.read.option("delimiter", "\t").csv(csv_temp)
-        .repartition(1)
-        .write.mode(SaveMode.Append).parquet(pq_path)
-      i = i + batch_lines
+    // df * loop
+    for (_ <- Range(0, loop, 1)) {
+      rdd = rdd union rdd
     }
+
+    // The schema is encoded in a string
+    val schemaString = Range(0, cols, 1).map(x => s"_c$x").mkString(",")
+
+    // Generate the schema based on the string of schema
+    val fields = schemaString.split(",")
+      .map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+
+    val rowRdd = rdd.flatMap(_.split("\n"))
+      .map(_.split("\t"))
+      .map(x=>{
+        Row.fromSeq(x.toSeq)
+      })
+    val df = spark.createDataFrame(rowRdd, schema)
+    df.coalesce(sinkBuckets)
+      .write.mode(SaveMode.Append).parquet(pqPath)
 
     spark.close()
   }
--- SparkDemo/src/main/sh/runParqGen.sh
+++ SparkDemo/src/main/sh/runParqGen.sh
@@ -12,31 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-csv_temp="hdfs://mycluster:9002/simon_test/csv_temp_t2"
-pq_path="hdfs://mycluster:9002/simon_test/t2"
-total_lines=86000000
-cols=155
 
-hadoop fs -mkdir -p $csv_temp
+# 1000 * 10 * 64 * 20 = 1280w, 20x 10g mem, parquet 140 G, 1hour
+sinkBuckets=20
+pqPath="hdfs://cs01:9002/simon_test/k4_test2"
+bucketsLines=1000
+genDup=10
+computeBuckets=20
+master=yarn
+cols=400
+loop=6  # pow(2, loop  ), 2^6 = 64
 
+source /home/disk1/dorisdb/.bashrc
 spark-submit --name ParqGen \
 --class com.dorisdb.spark.ParqGen \
 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
 --conf spark.sql.inMemoryColumnarStorage.compressed=true \
 --conf spark.speculation=false  \
---master local \
---driver-memory 30g \
---executor-memory 30g \
---num-executors 1 \
+--master $master \
+--driver-memory 5g \
+--executor-memory 10g \
+--num-executors $computeBuckets \
 --conf spark.driver.maxResultSize=4g \
 --conf spark.shuffle.manager=sort \
 --conf spark.shuffle.consolidateFiles=true \
 --conf spark.sql.sources.partitionColumnTypeInference.enabled=false  \
 --conf spark.driver.extraJavaOptions='-XX:+UseG1GC'  \
+--conf spark.executor.extraJavaOptions='-XX:+UseG1GC'  \
 --conf spark.memory.fraction=0.6 \
-./SparkParqGen.jar \
-$csv_temp \
-$pq_path \
-$total_lines \
-1000000 \
+/home/disk1/dorisdb/jars/SparkParqGen.v3.jar \
+$sinkBuckets \
+$pqPath \
+$bucketsLines \
+$genDup \
+$loop \
+$computeBuckets \
+$master \
 $cols
