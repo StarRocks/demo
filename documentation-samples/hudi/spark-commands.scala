8f8e0b312b47dcb770e9c078d24f5377bf1864db
DanRoscigno <dan@roscigno.com>
2024-02-12 10:54:45 -0500

    experiment with testing
    Signed-off-by: DanRoscigno <dan@roscigno.com>

---
+++ documentation-samples/hudi/spark-commands.scala
@@ -0,0 +1,41 @@
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SaveMode._
+import org.apache.hudi.DataSourceReadOptions._
+import org.apache.hudi.DataSourceWriteOptions._
+import org.apache.hudi.config.HoodieWriteConfig._
+import scala.collection.JavaConversions._
+
+val schema = StructType( Array(
+                 StructField("language", StringType, true),
+                 StructField("users", StringType, true),
+                 StructField("id", StringType, true)
+             ))
+
+val rowData= Seq(Row("Java", "20000", "a"), 
+               Row("Python", "100000", "b"), 
+               Row("Scala", "3000", "c"))
+
+
+val df = spark.createDataFrame(rowData,schema)
+
+val tableName = "hudi_coders_hive"
+val basePath = "s3a://huditest/hudi_coders"
+
+df.write.format("hudi").
+  option(org.apache.hudi.config.HoodieWriteConfig.TABLE_NAME, "hudi_coders_hive").
+  option(RECORDKEY_FIELD_OPT_KEY, "id").
+  option(PARTITIONPATH_FIELD_OPT_KEY, "language").
+  option(PRECOMBINE_FIELD_OPT_KEY, "users").
+  option("hoodie.datasource.write.hive_style_partitioning", "true").
+  option("hoodie.datasource.hive_sync.enable", "true").
+  option("hoodie.datasource.hive_sync.mode", "hms").
+  option("hoodie.datasource.hive_sync.database", "default").
+  option("hoodie.datasource.hive_sync.table", "hudi_coders_hive").
+  option("hoodie.datasource.hive_sync.partition_fields", "language").
+  option("hoodie.datasource.hive_sync.partition_extractor_class", "org.apache.hudi.hive.MultiPartKeysValueExtractor").
+  option("hoodie.datasource.hive_sync.metastore.uris", "thrift://hive-metastore:9083").
+  mode(Overwrite).
+  save(basePath)
+System.exit(0)
